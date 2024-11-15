120a42c5fbaed0b2cb03b8869a065c01133cb336
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2024-11-15 18:51:10 +0545

    authSlice and postForm problem resolved

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import Store from "./Store/Store.js";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import Home from "./Pages/Home.jsx";
 import Protected from "./Components/AuthLayout.jsx";
 import LoginPage from "./Pages/LoginPage.jsx";
@@ -17,65 +22,54 @@ import Post from "./Pages/Post.jsx";
 
 import AllPosts from "./Pages/AllPosts.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/login",
-        element: (
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route
+        path="login"
+        element={
           <Protected authentication={false}>
             <LoginPage />
           </Protected>
-        ),
-      },
-      {
-        path: "/signup",
-        element: (
+        }
+      />
+      <Route
+        path="signup"
+        element={
           <Protected authentication={false}>
             <SignUpPage />
           </Protected>
-        ),
-      },
-      {
-        path: "/all-posts",
-        element: (
-          <Protected authentication>
-            {" "}
+        }
+      />
+      <Route
+        path="all-posts"
+        element={
+          <Protected authentication={true}>
             <AllPosts />
           </Protected>
-        ),
-      },
-      {
-        path: "/add-post",
-        element: (
-          <Protected authentication>
-            {" "}
+        }
+      />
+      <Route
+        path="add-post"
+        element={
+          <Protected authentication={true}>
             <AddPost />
           </Protected>
-        ),
-      },
-      {
-        path: "/edit-post/:slug",
-        element: (
-          <Protected authentication>
-            {" "}
+        }
+      />
+      <Route
+        path="edit-post/:slug"
+        element={
+          <Protected authentication={true}>
             <EditPost />
           </Protected>
-        ),
-      },
-      {
-        path: "/post/:slug",
-        element: <Post />,
-      },
-    ],
-  },
-]);
+        }
+      />
+      <Route path="post/:slug" element={<Post />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
